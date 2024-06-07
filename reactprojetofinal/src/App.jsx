b592e9a97a643511eb837fcc66f8d2231f022656
jbasts <jonasbf.gomes@gmail.com>
2024-06-07 18:55:22 -0300

    NavBar e Footer

--- reactprojetofinal/src/App.jsx
+++ reactprojetofinal/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
 import Routes from './routes/Routes';
+import Footer from './components/Footer'; 
 import CartProvider from './context/CartContext';
+
 
 function App() {
   return (
     <CartProvider>
+      <Header></Header>
       <Routes />
+      <Footer></Footer>
     </CartProvider>
   );
 }
--- reactprojetofinal/src/main.jsx
+++ reactprojetofinal/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';  
+// import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+    <PrimeReactProvider>
     <App />
-  </React.StrictMode>,
-)
+    </PrimeReactProvider>
+</React.StrictMode>
+);
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
