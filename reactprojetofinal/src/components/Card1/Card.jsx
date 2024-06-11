9eb640620ecba031a9f5211a21c3215eb681883f
Marcos <marcosmattosnetto@hotmail.com>
2024-06-11 18:34:34 -0300

    carrinho sem localStorage

--- reactprojetofinal/src/components/Card1/Card.jsx
+++ reactprojetofinal/src/components/Card1/Card.jsx
@@ -1,23 +1,36 @@
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
 import { Cardbox, Container } from './style.js';
 
-export function Card() {
+export function Card(props) {
+  const produto = props.item
+  const { addToCart } = useContext(CartContext);
+
   return (
     <Cardbox>
       <img src="" alt="" />
-      <h2>Título do Produto</h2>
-      <h3>Descrição do Produto</h3>
-      <button>Adicionar ao Carrinho</button>
+      <h2>{produto.name}</h2>
+      <h3>{produto.descricao}</h3>
+      <h4>R$ {produto.price}</h4>
+      <button onClick={()=>addToCart(produto)}>Adicionar ao Carrinho</button>
     </Cardbox>
   );
 }
 
 export function CardList() {
+  const lista = [
+    {id:1,name:"sapato", descricao:"sapato",image:"",price:10.20},
+    {id:1,name:"Tênis", descricao:"sapato",image:"",price:10.20},
+    {id:1,name:"Camisa", descricao:"sapato",image:"",price:10.20},
+    {id:1,name:"Calça", descricao:"sapato",image:"",price:10.20}
+
+  ]
+
   return (
     <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {lista.map(item=> (
+      <Card item={item}/>
+    ))}
     </Container>
   );
 }
